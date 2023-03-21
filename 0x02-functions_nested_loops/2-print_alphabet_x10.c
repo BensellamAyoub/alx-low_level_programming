47f1139c0b8c89f2c7292fa478132616d91a7c83
AyoubBensellam <ayoubbensellam@gmail.com>
2023-03-21 08:06:37 -0700

    fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - abcs
+ *
+ * Return : always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
