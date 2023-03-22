7473385b6b9d4cc63c4dfb922108cef073575cd6
AyoubBensellam <ayoubbensellam@gmail.com>
2023-03-22 14:47:20 -0700

    fix

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - entry point
+*  : input
+* Return: 0 succes
+*/
+int main(void)
+{
+	int n;
+
+	int z;
+
+	int sum[50];
+
+	sum[0] = 1;
+	sum[1] = 2;
+	printf("1, 2, ");
+	for (n = 2; n < 50; n++)
+	{
+	z = sum[n - 1] + sum[n - 2];
+	printf("%i, ", z);
+	sum[n] = z;
+	}
+	return (0);
+}
