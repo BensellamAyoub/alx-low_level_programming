b4edf040112b2282ab55b492ef797de93e3f08d1
AyoubBensellam <ayoubbensellam@gmail.com>
2023-03-21 17:34:23 -0700

    fix

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - check last digit
+ * int : input
+ * Return: 1 is a letter 0 other
+ */
+int print_last_digit(int)
+{
+	int a;
+
+	a = a % 10;
+	_putchar('0' + a);
+}
