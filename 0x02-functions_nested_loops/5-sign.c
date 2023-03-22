91b14ada2038ddeafbe6f87e11056f756fb80257
AyoubBensellam <ayoubbensellam@gmail.com>
2023-03-21 17:15:34 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
  * print_sign - check sign 
- * @c : input
+ * @n : input
  * Return: 1 is a letter 0 other
  */
 int print_sign(int n)
 {
-	if (n > 0)	
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
-	}	
+	}
 	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	}	
+	}
 	else
-	{	
+	{
 		_putchar('-');
 		return (-1);
 	}
