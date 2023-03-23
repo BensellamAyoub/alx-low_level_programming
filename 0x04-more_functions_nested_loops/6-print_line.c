8a156d6fd13f0d7913f27bcaf207502bd283597c
AyoubBensellam <ayoubbensellam@gmail.com>
2023-03-23 14:14:19 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-		break;
+		return;
 	}
 
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
 		{
-			_putchar('#');
+			_putchar('_');
 		}
 		_putchar('\n');
 	}
