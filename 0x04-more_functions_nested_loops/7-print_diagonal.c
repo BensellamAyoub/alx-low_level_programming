e24cbea24a2bee5ec1a53fb0cb083663a60108a6
AyoubBensellam <ayoubbensellam@gmail.com>
2023-03-23 18:09:03 -0700

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,22 +6,20 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-
-	int j;
+	int i, j;
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+		_putchar(10);
 		return;
 	}
 	for (i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
-			_putchar(' ');
+			_putchar(32);
 		}
-		_putchar('\\');
-		_putchar('\n');
+		_putchar(92);
+		_putchar(10);
 	}
 }
