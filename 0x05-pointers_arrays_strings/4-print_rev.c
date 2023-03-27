12da3babb3c1ad26a3efed6a37e02ddeeb84fac6
AyoubBensellam <ayoubbensellam@gmail.com>
2023-03-27 16:24:25 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+/**
+ * print_rev - reverse
+ * @s : input
+ * Return: nothing
+ */
+#include "main.h"
+void print_rev(char *s)
+{
+	int i;
+
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	for (i = j; i <= j; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
