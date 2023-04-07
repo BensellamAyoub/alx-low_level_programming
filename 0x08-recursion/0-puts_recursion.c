82e8c026f9162d7ab60aba5152702b796116d1d7
AyoubBensellam <ayoubbensellam@gmail.com>
2023-04-06 17:26:43 -0700

    print with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _puts_recursion - string with recursion
+ * @s : input
+ * Return:  output.
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(*(s + i));
+		i++;
+		_puts_recursion(s + i);
+	}
+	_putchar('\n');
+}
