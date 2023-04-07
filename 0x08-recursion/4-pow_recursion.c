49ef69fc51d4edeb1f87a76cc4687800a0b25a03
AyoubBensellam <ayoubbensellam@gmail.com>
2023-04-06 18:58:42 -0700

    x to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _pow_recursion - x to power y
+ * @x : input
+ * @y : input
+ * Return:  int x power y.
+ */
+#include "main.h"
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
