d62a670daebae03f659ef0ac171523970089d0b8
AyoubBensellam <ayoubbensellam@gmail.com>
2023-04-11 17:47:24 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,8 +16,10 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	while (str != '\0')
+	while (str[len] != '\0')
+	{
 		len++;
+	}
 	arr = malloc((len + 1) * sizeof(char));
 
 	if (arr == NULL)
