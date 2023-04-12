bc3b798e1d386205666cd1b5502d7993b884637d
AyoubBensellam <ayoubbensellam@gmail.com>
2023-04-11 17:44:21 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+/**
+ * _strdup - duplicate string
+ * @str : input
+ * Return: array
+ */
+char *_strdup(char *str)
+{
+	int len = 0;
+
+	int i = 0;
+
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str != '\0')
+		len++;
+	arr = malloc((len + 1) * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		arr[i] = str[i];
+	}
+	return (arr);
+}
