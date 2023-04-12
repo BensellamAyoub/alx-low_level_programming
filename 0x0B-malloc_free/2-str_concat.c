5c67f8d9aa0feedb728c455d0b9508d82313f929
AyoubBensellam <ayoubbensellam@gmail.com>
2023-04-11 18:20:16 -0700

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,11 +35,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < len_s1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s3[i] = s1[i];
 	}
-	for (j = 0; j < len_s2; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 	{
 		s3[i] = s2[j];
 		i++;
