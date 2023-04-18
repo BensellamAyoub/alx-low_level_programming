d15e95451119087da68201db5ca754d681f611de
AyoubBensellam <ayoubbensellam@gmail.com>
2023-04-17 17:08:29 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -  dog's elements
+ * @name: element 1 
+ * @age: element 2
+ * @owner: elment 3
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
