ddc00bcac966d1811a97e307fb412804ac46e22a
AyoubBensellam <ayoubbensellam@gmail.com>
2023-04-27 18:28:22 -0700

    ok

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int factorial(int n);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
@@ -6,3 +9,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 void _puts_recursion(char *s);
+#endif
